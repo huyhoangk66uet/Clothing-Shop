d1ce031c067baf9a110cfab5b793ddd92899bc7c
Nguyen Huy <21020765@vnu.edu.vn>
2023-04-28 01:11:34 +0700

    update home

--- src/app/controllers/HomeController.js
+++ src/app/controllers/HomeController.js
@@ -3,30 +3,39 @@ import Product from '../models/Product.js'; // tra ve bang users
 
 
 class HomeController {
-    
+
     // [GET] /home
     show(req, res, next) {
 
         var check_out_auth = false
-        try{var token = req.cookies.token;
-            var ketqua = jwt.verify(token,'mk');
+        try {
+            var token = req.cookies.token;
+            var ketqua = jwt.verify(token, 'mk');
         } catch {
 
         }
-        if(!ketqua) {
+        if (!ketqua) {
             check_out_auth = true
         }
 
-        Product.find({}) 
-                .then(products => {
-                    products = products.map(product => product.toObject())
-                    res.render('./home', {check_out_auth, products})
-                })
-                .catch(err => {
-                    res.status(400).json({ error: err })
-                })
-
-           
+        var outstandingProducts;
+        Product.find({ isDelete: false, isOutstanding: true })
+            .then(outstandingProducts => {
+                outstandingProducts = outstandingProducts.map(product => product.toObject())
+                Product.find({ isDelete: false })
+                    .sort({ _id: -1 })
+                    .limit(10)
+                    .then(newProducts => {
+                        newProducts = newProducts.map(product => product.toObject())
+                        res.render('./home', { check_out_auth, newProducts, outstandingProducts })
+                    })
+                    .catch(err => {
+                        res.status(400).json({ error: err })
+                    })
+            })
+            .catch(err => {
+                res.status(400).json({ error: err })
+            })
     }
 
 
@@ -34,7 +43,7 @@ class HomeController {
         console.log(req.session.user_name)
         res.clearCookie('token');
         req.session.destroy((err) => {
-            if(err) {
+            if (err) {
                 console.log(err);
             } else {
                 //res.render('./home')
@@ -43,7 +52,7 @@ class HomeController {
         })
     }
 
-    
+
 }
 
 // exports cai gi thi khi require se nhan duoc cai do
