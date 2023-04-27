74d5d15ee5f161256796dd7a88de21139afb09b0
hoang1803 <21020073@vnu.edu.vn>
2023-04-27 10:49:57 +0700

    Update profile.router.js
    update /phone, email, pass, info

--- src/routes/profile.router.js
+++ src/routes/profile.router.js
@@ -5,10 +5,10 @@ import profileController from '../app/controllers/ProfileController.js'
 
 
 
-router.get('/phone', profileController.redirectPath)
-router.get('/email', profileController.redirectPath)
-router.get('/password', profileController.redirectPath)
-router.get('/info', profileController.show)
+router.get('/phone', profileController.phone)
+router.get('/email', profileController.email)
+router.get('/password', profileController.password)
+router.get('/info', profileController.info)
 router.get('/:page', profileController.redirectPath)
 router.get('/', profileController.redirectPath)
 
